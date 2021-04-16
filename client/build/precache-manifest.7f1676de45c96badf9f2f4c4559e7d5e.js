4c31179fcfd9db8f592e30924a067f2eec087178
Sahaj Shrestha <shresthas6@wit.edu>
2021-04-16 02:53:49 -0400

    here

---
+++ client/build/precache-manifest.7f1676de45c96badf9f2f4c4559e7d5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92af118fe917fc57885432e7b183bc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf43fecabca18291753",
+    "url": "/static/css/main.a155a8cd.chunk.css"
+  },
+  {
+    "revision": "1e72de6ab4067451ac2d",
+    "url": "/static/js/2.a911761e.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.a911761e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf43fecabca18291753",
+    "url": "/static/js/main.6b131041.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5f01fd22b1a8901c116d410e96f2ab2f",
+    "url": "/static/media/memories.5f01fd22.png"
+  },
+  {
+    "revision": "7c8b0515b37b52230a09e775383f9677",
+    "url": "/static/media/witimage.7c8b0515.jpg"
+  }
+]);
